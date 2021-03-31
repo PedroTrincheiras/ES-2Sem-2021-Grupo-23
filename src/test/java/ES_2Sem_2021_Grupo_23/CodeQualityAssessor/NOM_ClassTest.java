4df49209117d0ec6994f8a996191a393bfc0030b
TiagoMAGarcia <73131074+TiagoMAGarcia@users.noreply.github.com>
2021-03-31 17:29:48 +0100

    NOM_Class and NOM_ClassTest code implemented

---
+++ src/test/java/ES_2Sem_2021_Grupo_23/CodeQualityAssessor/NOM_ClassTest.java
@@ -0,0 +1,61 @@
+package ES_2Sem_2021_Grupo_23.CodeQualityAssessor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.javaparser.utils.Pair;
+
+class NOM_ClassTest {
+
+	@Test
+	void GrammarException() {
+		File file = new File("jasmlFiles\\com\\jasml\\compiler\\GrammerException.java");
+		List<Pair<String, Integer>> numberOfMethods = NOM_Class.getNOM(file);
+		List<Pair<String, Integer>> expectedNumberOfMethods = new ArrayList<Pair<String,Integer>>();
+		expectedNumberOfMethods.add(new Pair<String, Integer>("GrammerException", 4));
+		assertEquals(expectedNumberOfMethods, numberOfMethods);
+	}
+	
+	@Test
+	void SourceCodeParser() {
+		File file = new File("jasmlFiles\\com\\jasml\\compiler\\SourceCodeParser.java");
+		List<Pair<String, Integer>> numberOfMethods = NOM_Class.getNOM(file);
+		List<Pair<String, Integer>> expectedNumberOfMethods = new ArrayList<Pair<String,Integer>>();
+		expectedNumberOfMethods.add(new Pair<String, Integer>("OpcodeWrapper", 2));
+		expectedNumberOfMethods.add(new Pair<String, Integer>("LabeledInstructions", 1));
+		expectedNumberOfMethods.add(new Pair<String, Integer>("SourceCodeParser", 29));
+		assertEquals(expectedNumberOfMethods, numberOfMethods);
+	}
+	
+	@Test
+	void ParsingException() {
+		File file = new File("jasmlFiles\\com\\jasml\\compiler\\ParsingException.java");
+		List<Pair<String, Integer>> numberOfMethods = NOM_Class.getNOM(file);
+		List<Pair<String, Integer>> expectedNumberOfMethods = new ArrayList<Pair<String,Integer>>();
+		expectedNumberOfMethods.add(new Pair<String, Integer>("ParsingException", 6));
+		assertEquals(expectedNumberOfMethods, numberOfMethods);
+	}
+	
+	@Test
+	void ConstantPoolGenerator() {
+		File file = new File("jasmlFiles\\com\\jasml\\compiler\\ConstantPoolGenerator.java");
+		List<Pair<String, Integer>> numberOfMethods = NOM_Class.getNOM(file);
+		List<Pair<String, Integer>> expectedNumberOfMethods = new ArrayList<Pair<String,Integer>>();
+		expectedNumberOfMethods.add(new Pair<String, Integer>("ConstantPoolGenerator", 25));
+		assertEquals(expectedNumberOfMethods, numberOfMethods);
+	}
+	
+	@Test
+	void Undefined() {
+		File file = new File("Undefined");
+		List<Pair<String, Integer>> numberOfMethods = NOM_Class.getNOM(file);
+		assertNull(numberOfMethods);
+	}
+
+
+}
