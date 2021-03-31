c850f7509fcd628d31de7c13873d5abc58e5feca
Filipe <filipebarroso2812@gmail.com>
2021-03-31 19:41:57 +0100

    create LOC_Class metric

--- src/main/java/ES_2Sem_2021_Grupo_23/CodeQualityAssessor/Metrics/LOC_Class.java
+++ src/main/java/ES_2Sem_2021_Grupo_23/CodeQualityAssessor/Metrics/LOC_Class.java
@@ -1,5 +1,39 @@
 package ES_2Sem_2021_Grupo_23.CodeQualityAssessor.Metrics;
 
-public class LOC_Class {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.utils.Pair;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
+	
+
+public class LOC_Class {
+	
+	public static List<Pair<String, Integer>> getLOC_Class(File file) {
+		List<Pair<String, Integer>> classes = new ArrayList<Pair<String, Integer>>();
+		try {
+			CompilationUnit cu = StaticJavaParser.parse(file);
+			LexicalPreservingPrinter.setup(cu);
+			ClassOrInterface coi = new ClassOrInterface();	
+			coi.visit(cu, classes);
+		} catch (FileNotFoundException e) {
+			return null;
+		}
+		return classes;
+	}
+	
+	private static class ClassOrInterface extends VoidVisitorAdapter<List<Pair<String, Integer>>> {
+
+		@Override
+		public void visit(ClassOrInterfaceDeclaration coid, List<Pair<String, Integer>> collector) {
+			super.visit(coid, collector);
+			collector.add(new Pair<String, Integer>(coid.getNameAsString(), LexicalPreservingPrinter.print(coid).split("\n").length));
+		}
+	}	
 }
