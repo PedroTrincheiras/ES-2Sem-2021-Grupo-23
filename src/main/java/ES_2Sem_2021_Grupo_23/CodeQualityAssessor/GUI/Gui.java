e5e358f21769881ab53a3d244837e7338396279a
PedroTrincheiras <59057189+PedroTrincheiras@users.noreply.github.com>
2021-04-02 15:00:41 +0100

    Design Update

--- src/main/java/ES_2Sem_2021_Grupo_23/CodeQualityAssessor/GUI/Gui.java
+++ src/main/java/ES_2Sem_2021_Grupo_23/CodeQualityAssessor/GUI/Gui.java
@@ -1,11 +1,14 @@
 package ES_2Sem_2021_Grupo_23.CodeQualityAssessor.GUI;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+
 import java.awt.event.*;
 import java.io.*;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.SystemColor;
+import java.awt.Color;
 
 public class Gui extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 7448241899663378705L;
@@ -16,30 +19,42 @@ public class Gui extends JFrame implements ActionListener {
 	private JButton exportButton;
 
 	public Gui() {
+		setTitle("Metric Exporter");
 		getContentPane().setLayout(null);
-		
+		getContentPane().setBackground(new Color(255, 255, 255));
 		choose = new JButton("Select Directory");
-		choose.setBounds(324, 76, 150, 23);
+		choose.setForeground(new Color(255, 255, 255));
+		choose.setBackground(new Color(52, 73, 94));
+		choose.setBounds(324, 64, 150, 35);
 		choose.addActionListener(this);
+		choose.setBorder(null);
 		getContentPane().add(choose);
 		
 		cDirectory = new JTextField();
-		cDirectory.setBackground(SystemColor.controlHighlight);
-		cDirectory.setBounds(10, 76, 304, 23);
+		cDirectory.setBackground(new Color(255, 255, 255));
+		cDirectory.setBounds(10, 64, 304,35);
+		cDirectory.setBorder(new LineBorder(new Color(189, 195, 199)));
 		getContentPane().add(cDirectory);
 		
 		export = new JButton("Export Directory");
-		export.setBounds(324, 127, 150, 23);
+		export.setForeground(new Color(255, 255, 255));
+		export.setBackground(new Color(52, 73, 94));
+		export.setBounds(324, 110, 150, 35);
 		export.addActionListener(this);
+		export.setBorder(null);
 		getContentPane().add(export);
 		
 		eDirectory = new JTextField();
-		eDirectory.setBackground(SystemColor.controlHighlight);
-		eDirectory.setBounds(10, 127, 304, 23);
+		eDirectory.setBackground(new Color(255, 255, 255));
+		eDirectory.setBounds(10, 110, 304, 35);
+		eDirectory.setBorder(new LineBorder(new Color(189, 195, 199)));
 		getContentPane().add(eDirectory);
 		
 		exportButton = new JButton("Export");
-		exportButton.setBounds(10, 206, 464, 23);
+		exportButton.setForeground(new Color(255, 255, 255));
+		exportButton.setBackground(new Color(52, 73, 94));
+		exportButton.setBounds(10, 195, 464, 35);
+		exportButton.setBorder(null);
 		getContentPane().add(exportButton);
 	}
 
@@ -74,7 +89,7 @@ public class Gui extends JFrame implements ActionListener {
 	
 	public static void main(String[] args) {
 		Gui gui = new Gui();
-		gui.setSize(500, 300);
+		gui.setSize(500, 280);
 		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
 	    int x = (int) ((dimension.getWidth() - gui.getWidth()) / 2);
 	    int y = (int) ((dimension.getHeight() - gui.getHeight()) / 2);
