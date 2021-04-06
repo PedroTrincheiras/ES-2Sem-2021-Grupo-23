ba66e6b60e021da58451b6b1a99271994011e9f8
Tomás Rodrigues <tomasdcrodrigues@gmail.com>
2021-04-06 14:35:44 +0100

    generate xlsx fixed

--- src/test/java/ES_2Sem_2021_Grupo_23/CodeQualityAssessor/Generate_CLSC_With_Metrics/Generate_XLSX_With_MetricsTest.java
+++ src/test/java/ES_2Sem_2021_Grupo_23/CodeQualityAssessor/Generate_CLSC_With_Metrics/Generate_XLSX_With_MetricsTest.java
@@ -1,17 +1,12 @@
 package ES_2Sem_2021_Grupo_23.CodeQualityAssessor.Generate_CLSC_With_Metrics;
 
-import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import ES_2Sem_2021_Grupo_23.CodeQualityAssessor.Generate_XLSX_With_Metrics.Generate_XLSX_With_Metrics;
@@ -20,7 +15,7 @@ class Generate_XLSX_With_MetricsTest {
 
 	@Test
 	void testGenerateXLSX() {
-		String folder = ("C:/Users/tomas/PycharmProjects/ES-2Sem-2021-Grupo-23/jasmlFiles");
+		String folder = ("jasmlFiles\\");
 		FileInputStream f;
 		Boolean equal=true;
 		try {
