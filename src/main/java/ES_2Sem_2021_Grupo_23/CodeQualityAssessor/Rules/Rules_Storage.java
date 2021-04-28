b2b5bd379ffaaf2c6b538df6750f1b32a2796787
Filipe <filipebarroso2812@gmail.com>
2021-04-28 15:37:52 +0100

    javadocs added to rules_storage

--- src/main/java/ES_2Sem_2021_Grupo_23/CodeQualityAssessor/Rules/Rules_Storage.java
+++ src/main/java/ES_2Sem_2021_Grupo_23/CodeQualityAssessor/Rules/Rules_Storage.java
@@ -7,22 +7,47 @@ public class Rules_Storage {
 
 	private HashMap<String, String> rules;
 	
+	/**
+	 * Create a new Rule_Storage without any parameter
+	 */
 	public Rules_Storage() {
 		rules = new HashMap<String, String>();
 	}
 	
+	/**
+	 * Create a new Rule Storage
+	 * @param file - to create a new Rule_Storage with the existing rules in the file
+	 */
 	public Rules_Storage(File file) {
 		// TODO 
 	}
 	
+	/**
+	 * Give all rule names
+	 * 
+	 * @return all the names of the existing rules in the Rule_Storage
+	 */
 	public Set<String> getRulesNames() {
 		return this.rules.keySet();
 	}
 	
+	/**
+	 * Receives a rule name and return the corresponding rule
+	 * 
+	 * @param String - to search the rule in the Rule_Storage
+	 * @return the respective rule
+	 */
 	public String getRule(String ruleName) {
 		return this.rules.get(ruleName);
 	}
 	
+	/**
+	 * Add a new rule to the Rule_Storage if it doesn't already exists
+	 * 
+	 * @param String - to check if the rule already exists and if not to use as hashmap key
+	 * @param String - rule content
+	 * @return if the rule already exits return false, else return true
+	 */
 	public boolean addRule(String ruleName, String rule) {
 		for (String name : this.rules.keySet()) {
 			if(name.equals(ruleName)) {
