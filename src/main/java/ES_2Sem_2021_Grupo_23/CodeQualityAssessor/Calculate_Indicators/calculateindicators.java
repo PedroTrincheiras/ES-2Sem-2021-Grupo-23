1c9e3c45411c90f01a97d9853f65c4b40cd49556
Diogo Mano <dfjmo@iscte-iul.pt>
2021-05-04 18:33:17 +0100

    calculate indicators update
    to do list:
    java docs

--- src/main/java/ES_2Sem_2021_Grupo_23/CodeQualityAssessor/Calculate_Indicators/calculateindicators.java
+++ src/main/java/ES_2Sem_2021_Grupo_23/CodeQualityAssessor/Calculate_Indicators/calculateindicators.java
@@ -15,10 +15,19 @@ import com.github.javaparser.utils.Pair;
 import ES_2Sem_2021_Grupo_23.CodeQualityAssessor.CodeSmell_Editor.CodeSmell_Editor;
 
 public class calculateindicators {
+	/**
+	 * 
+	 * @param directory
+	 * @param rule
+	 * @param ruleName
+	 * @return
+	 * @throws IOException
+	 * @throws ScriptException
+	 */
 
 	public static List<Pair<String, String>> function(String directory,String rule,String ruleName) throws IOException, ScriptException {
 		List<Pair<String, String>> list = new ArrayList<Pair<String, String>>();
-		ruleName = rule.toLowerCase();
+		ruleName = ruleName.toLowerCase();
 
 		FileInputStream f = new FileInputStream(new File(directory));
 		try (XSSFWorkbook workbook = new XSSFWorkbook(f)) {
@@ -38,30 +47,46 @@ public class calculateindicators {
 					int WMC_class = (int) workbook.getSheet("Metrics").getRow(i).getCell(6).getNumericCellValue();
 					int LOC_method = (int) workbook.getSheet("Metrics").getRow(i).getCell(7).getNumericCellValue();
 					int CYCLO_method = (int) workbook.getSheet("Metrics").getRow(i).getCell(8).getNumericCellValue();
+					if(workbook.getSheet("Metrics").getRow(i).getCell(column).equals(null)) continue;
 					boolean manualValue =  workbook.getSheet("Metrics").getRow(i).getCell(column).getBooleanCellValue();
 					boolean autoValue = CodeSmell_Editor.codeSmellIdentifier(rule, LOC_method, CYCLO_method, LOC_class, NOM_class, WMC_class);
-					
-					
+					if(manualValue && autoValue) list.add(new Pair<String,String>(methodID, "VP"));
+					else if(!manualValue && autoValue) list.add(new Pair<String,String>(methodID, "FP"));
+					else if(!manualValue && !autoValue) list.add(new Pair<String,String>(methodID, "VN"));
+					else if(manualValue && !autoValue) list.add(new Pair<String,String>(methodID, "FN"));
+				}
+			}else {
+				for (int i = 1; i < workbook.getSheet("Metrics").getLastRowNum(); i++) {
+					String className = workbook.getSheet("Metrics").getRow(i).getCell(2).toString();
+					if(containFirstElement(list, className)) continue;
+					int NOM_class = (int) workbook.getSheet("Metrics").getRow(i).getCell(4).getNumericCellValue();
+					int LOC_class = (int) workbook.getSheet("Metrics").getRow(i).getCell(5).getNumericCellValue();
+					int WMC_class = (int) workbook.getSheet("Metrics").getRow(i).getCell(6).getNumericCellValue();
+					int LOC_method = (int) workbook.getSheet("Metrics").getRow(i).getCell(7).getNumericCellValue();
+					int CYCLO_method = (int) workbook.getSheet("Metrics").getRow(i).getCell(8).getNumericCellValue();
+					if(workbook.getSheet("Metrics").getRow(i).getCell(column).equals(null)) continue;
+					boolean manualValue =  workbook.getSheet("Metrics").getRow(i).getCell(column).getBooleanCellValue();
+					boolean autoValue = CodeSmell_Editor.codeSmellIdentifier(rule, LOC_method, CYCLO_method, LOC_class, NOM_class, WMC_class);
+					if(manualValue && autoValue) list.add(new Pair<String,String>(className, "VP"));
+					else if(!manualValue && autoValue) list.add(new Pair<String,String>(className, "FP"));
+					else if(!manualValue && !autoValue) list.add(new Pair<String,String>(className, "VN"));
+					else if(manualValue && !autoValue) list.add(new Pair<String,String>(className, "FN"));
+				}
 			}
-		}else {
-			for (int i = 1; i < workbook.getSheet("Metrics").getLastRowNum(); i++) {
-				String className = workbook.getSheet("Metrics").getRow(i).getCell(2).toString();
-
-				int NOM_class = (int) workbook.getSheet("Metrics").getRow(i).getCell(4).getNumericCellValue();
-				int LOC_class = (int) workbook.getSheet("Metrics").getRow(i).getCell(5).getNumericCellValue();
-				int WMC_class = (int) workbook.getSheet("Metrics").getRow(i).getCell(6).getNumericCellValue();
-				int LOC_method = (int) workbook.getSheet("Metrics").getRow(i).getCell(7).getNumericCellValue();
-				int CYCLO_method = (int) workbook.getSheet("Metrics").getRow(i).getCell(8).getNumericCellValue();
-				boolean manualValue =  workbook.getSheet("Metrics").getRow(i).getCell(column).getBooleanCellValue();
-				boolean autoValue = CodeSmell_Editor.codeSmellIdentifier(rule, LOC_method, CYCLO_method, LOC_class, NOM_class, WMC_class);
-			}
-		}
 	}
 		return list;
 	}
-	
-	public static void main(String[] args) throws IOException {
-		function("C:/Users/tomas/Documents/GitHub/ES-2Sem-2021-Grupo-23/jasmlFiles/jasmlFiles_metrics.xlsx","CYCLO_method<3","method");
+	/**
+	 * 
+	 * @param list
+	 * @param compare
+	 * @return
+	 */
+	public static boolean containFirstElement(List<Pair<String, String>> list, String compare) {
+		for(Pair<String, String> l : list) {
+			if(l.a.equals(compare)) return true;
+		}
+		return false;
 	}
 
 }
