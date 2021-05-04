ded2d1dadce074aaaa2771108694332cf7cbc072
Tomás Rodrigues <tdcrs@iscte-iul.pt>
2021-05-04 22:17:02 +0100

    Update CalculateIndicators.java
    add function countIndicators
    fix methos and class name
    complete javadocs
    add coments

--- src/main/java/ES_2Sem_2021_Grupo_23/CodeQualityAssessor/Calculate_Indicators/CalculateIndicators.java
+++ src/main/java/ES_2Sem_2021_Grupo_23/CodeQualityAssessor/Calculate_Indicators/CalculateIndicators.java
@@ -14,7 +14,11 @@ import com.github.javaparser.utils.Pair;
 
 import ES_2Sem_2021_Grupo_23.CodeQualityAssessor.CodeSmell_Editor.CodeSmell_Editor;
 
-public class calculateindicators {
+/**
+ * @author tomas,dimano
+ *
+ */
+public class CalculateIndicators {
 	/**
 	 * 
 	 * @param directory
@@ -25,7 +29,7 @@ public class calculateindicators {
 	 * @throws ScriptException
 	 */
 
-	public static List<Pair<String, String>> function(String directory,String rule,String ruleName) throws IOException, ScriptException {
+	public static List<Pair<String, String>> getIndicators(String directory,String rule,String ruleName) throws IOException, ScriptException {
 		List<Pair<String, String>> list = new ArrayList<Pair<String, String>>();
 		ruleName = ruleName.toLowerCase();
 
@@ -37,6 +41,7 @@ public class calculateindicators {
 					column=j;
 				}
 			}
+			//return null if the rulename dont exist in the xlsx file
 			if(column==0)return null;
 			if (ruleName.contains("method")) {
 				for (int i = 1; i < workbook.getSheet("Metrics").getLastRowNum(); i++) {
@@ -47,6 +52,7 @@ public class calculateindicators {
 					int WMC_class = (int) workbook.getSheet("Metrics").getRow(i).getCell(6).getNumericCellValue();
 					int LOC_method = (int) workbook.getSheet("Metrics").getRow(i).getCell(7).getNumericCellValue();
 					int CYCLO_method = (int) workbook.getSheet("Metrics").getRow(i).getCell(8).getNumericCellValue();
+					//continue for the next iteration if the cell is undefined
 					if(workbook.getSheet("Metrics").getRow(i).getCell(column).equals(null)) continue;
 					boolean manualValue =  workbook.getSheet("Metrics").getRow(i).getCell(column).getBooleanCellValue();
 					boolean autoValue = CodeSmell_Editor.codeSmellIdentifier(rule, LOC_method, CYCLO_method, LOC_class, NOM_class, WMC_class);
@@ -58,12 +64,14 @@ public class calculateindicators {
 			}else {
 				for (int i = 1; i < workbook.getSheet("Metrics").getLastRowNum(); i++) {
 					String className = workbook.getSheet("Metrics").getRow(i).getCell(2).toString();
+					//if the classe already exist in the list continue for the next line
 					if(containFirstElement(list, className)) continue;
 					int NOM_class = (int) workbook.getSheet("Metrics").getRow(i).getCell(4).getNumericCellValue();
 					int LOC_class = (int) workbook.getSheet("Metrics").getRow(i).getCell(5).getNumericCellValue();
 					int WMC_class = (int) workbook.getSheet("Metrics").getRow(i).getCell(6).getNumericCellValue();
 					int LOC_method = (int) workbook.getSheet("Metrics").getRow(i).getCell(7).getNumericCellValue();
 					int CYCLO_method = (int) workbook.getSheet("Metrics").getRow(i).getCell(8).getNumericCellValue();
+					//continue for the next iteration if the cell is undefined
 					if(workbook.getSheet("Metrics").getRow(i).getCell(column).equals(null)) continue;
 					boolean manualValue =  workbook.getSheet("Metrics").getRow(i).getCell(column).getBooleanCellValue();
 					boolean autoValue = CodeSmell_Editor.codeSmellIdentifier(rule, LOC_method, CYCLO_method, LOC_class, NOM_class, WMC_class);
@@ -88,5 +96,32 @@ public class calculateindicators {
 		}
 		return false;
 	}
-
+	
+	
+	/** receive a list of pairs of string where first element is the methodid or classename and the second is the indicator(VP,FP,VN,FN)
+	 * and return of presences for each indicator
+	 * @param list
+	 * @return
+	 */
+	public static List<Pair<String,Integer>> countIndicators(List<Pair<String, String>> list){
+		List<Pair<String,Integer>> result_list=new ArrayList<>();
+		int vp=0,fp=0,vn=0,fn=0;
+		for(Pair<String, String> l:list) {
+			if(l.b.equals("VP")) {
+				vp++;
+			}else if(l.b.equals("FP")) {
+				fp++;
+			}else if(l.b.equals("VN")) {
+				vn++;
+			}else if(l.b.equals("FN")) {
+				fn++;
+			}
+			
+		}
+		result_list.add(new Pair<String,Integer>("VP",vp));
+		result_list.add(new Pair<String,Integer>("FP",fp));
+		result_list.add(new Pair<String,Integer>("VN",vn));
+		result_list.add(new Pair<String,Integer>("FP",fn));
+		return result_list;		
+	}
 }
